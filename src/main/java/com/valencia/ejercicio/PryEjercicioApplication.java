88ce937f4b02af2357e58a14faf27a53c8481f00
odvalencia1 <65565908+odvalencia1@users.noreply.github.com>
2020-05-20 18:20:31 -0500

    Codigo Inicial

---
+++ src/main/java/com/valencia/ejercicio/PryEjercicioApplication.java
@@ -0,0 +1,13 @@
+package com.valencia.ejercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PryEjercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PryEjercicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valencia/ejercicio/PryEjercicioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valencia.ejercicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PryEjercicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
